f4f5fda6d75c4044a86dc7410b796fba2113c7b0
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2015-03-04 15:08:45 +0100

    Added new-panel (work in progress). Updated Jsorolla

--- src/t-config.js
+++ src/t-config.js
@@ -1,7 +1,8 @@
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://www.ebi.ac.uk/cellbase/webservices/rest"
 //CELLBASE_HOST = "http://ws-beta.bioinfo.cipf.es/cellbase/rest";
-OPENCGA_HOST = "http://ws-beta.bioinfo.cipf.es/opencga-staging/rest";
+//OPENCGA_HOST = "http://ws-beta.bioinfo.cipf.es/opencga-staging/rest";
+OPENCGA_HOST = "http://test.babelomics.org/opencga/rest";
 
 EXAMPLE_PANELS = [
     {
