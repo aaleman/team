39b54e048f192b7a8ae715e5c461d08dfd38ebbc
Alejandro Alemán <aaleman@cipf.es>
2016-04-05 12:06:13 +0200

    Add new parameter 'opencga-home' to Parameters

--- team-web/conf/config.js
+++ team-web/conf/config.js
@@ -5,7 +5,6 @@ CELLBASE_VERSION = "v3";
 // CELLBASE_VERSION = "v4";
 
 OPENCGA_HOST = "http://test.babelomics.org/opencga-0.7-dev";
-// OPENCGA_HOST = "http://bioinfoint.hpc.cam.ac.uk/opencga/rest";
 
 CONSEQUENCE_TYPES = [{
     "name": "transcript_ablation",
