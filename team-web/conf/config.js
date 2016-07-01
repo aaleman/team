45b91e5be6576fe4e93f5500724c07741f809f9c
frasator <frasator@gmail.com>
2016-07-01 14:45:03 +0200

    Project updates, WIP

--- team-web/conf/config.js
+++ team-web/conf/config.js
@@ -1,8 +1,8 @@
-// CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
-// CELLBASE_VERSION = "v3";
+CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
+CELLBASE_VERSION = "v3";
 
-CELLBASE_HOST = "http://bioinfoint.hpc.cam.ac.uk/cellbase";
-CELLBASE_VERSION = "v4";
+// CELLBASE_HOST = "http://bioinfoint.hpc.cam.ac.uk/cellbase";
+// CELLBASE_VERSION = "v4";
 
 OPENCGA_HOST = "http://test.babelomics.org/opencga-0.7-dev";
 
